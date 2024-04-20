1cba3ed7cdbcad2fe07b1e11c50ccf56fd4e79d0
Spencer Dent <mr.spencerdent@gmail.com>
2024-04-20 15:39:50 -0400

    feat: read & echo static file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error};
+
+fn main() -> Result<(), Error> {
+    let path = "us_customers_mapAssign.csv";
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        print!("{}\n", line?);
+    }
+
+    Ok(())
 }
